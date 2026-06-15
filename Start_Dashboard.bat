bb975d0a17d118b6ac5c69a7ed1a9ac6c82e7ba3
Dr Arkaprabha Sau <33415109+arka1985@users.noreply.github.com>
2026-06-16 01:44:43 +0530

    Fix dashboard script paths and add npm install

--- Start_Dashboard.bat
+++ Start_Dashboard.bat
@@ -3,10 +3,17 @@ echo Starting PACS Dashboard...
 echo Please wait while the development server starts.
 echo The browser will open automatically.
 
-cd /d "c:\Users\arka\ILO_PACS\pacs-dashboard"
+:: Change directory to the pacs-dashboard folder relative to where this script is located
+cd /d "%~dp0pacs-dashboard"
 
 :: Clean up Next.js cache to avoid corruption causing OOM
-if exist "c:\Users\arka\ILO_PACS\pacs-dashboard\.next" rmdir /s /q "c:\Users\arka\ILO_PACS\pacs-dashboard\.next"
+if exist ".next" rmdir /s /q ".next"
+
+:: Install dependencies if they are missing
+if not exist "node_modules\" (
+    echo Installing dependencies for the first time... Please wait.
+    npm install
+)
 
 :: Increase Node.js memory limit to 8GB to prevent heap out of memory errors
 set NODE_OPTIONS=--max_old_space_size=8192
